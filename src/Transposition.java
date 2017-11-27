cf35a526af5acaa46bfe7dc2ade87cac045b2e17
Elio <eliocamison@gmail.com>
2017-11-27 22:20:19 +0100

    Transposition - 1%

--- src/Transposition.java
+++ src/Transposition.java
@@ -1,7 +1,20 @@
-
+import java.util.Arrays;
 
 public class Transposition {
+
+    public static void main(String[] args) {
+        System.out.println(cypher("ABCDEFG", 2));
+    }
+
     static String cypher(String s, int dim) {
+        s.toUpperCase();
+        char[][] matriu = new char[2][];
+
+        for (int i = 0; i < s.length(); i++) {
+            matriu[i][i] = s.charAt(i);
+        }
+
+        System.out.println(Arrays.toString(matriu));
         return "";
     }
 
