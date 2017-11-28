cb37ee8221baa7650f7f8f51e86efc3edaa213d9
Elio <eliocamison@gmail.com>
2017-11-28 21:55:28 +0100

    Transposition - error

--- src/Transposition.java
+++ src/Transposition.java
@@ -3,18 +3,19 @@ import java.util.Arrays;
 public class Transposition {
 
     public static void main(String[] args) {
+
         System.out.println(cypher("ABCDEFG", 2));
     }
 
     static String cypher(String s, int dim) {
         s.toUpperCase();
-        char[][] matriu = new char[2][];
 
-        for (int i = 0; i < s.length(); i++) {
-            matriu[i][i] = s.charAt(i);
+        for (int i = 0; i <s.length() ; i++) {
+            char[] matriu = new char[s.charAt(i)];
+
         }
+        //char[] matriu = s.toCharArray();
 
-        System.out.println(Arrays.toString(matriu));
         return "";
     }
 
