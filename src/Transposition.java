014572dc2160da2472583535eb9e6a606f97bedd
Elio <eliocamison@gmail.com>
2017-12-04 23:04:17 +0100

    Transposition - :(

--- src/Transposition.java
+++ src/Transposition.java
@@ -9,8 +9,8 @@ public class Transposition {
     static String cypher(String s, int dim) {
         int alt = s.length() / dim;
         if (s.length() / dim != 0) { alt++; }
-
         char[][] matriu = new char[alt][dim];
+        StringBuilder result = new StringBuilder();
 
         int pos = 0;
         for (int i = 0; i < alt; i++) {
@@ -22,10 +22,29 @@ public class Transposition {
             }
         }
 
-        System.out.println(Arrays.deepToString(matriu));
-
-        return "";
+        result.append(matriu[0][0]);
+        result.append(matriu[1][0]);
+        result.append(matriu[2][0]);
+        result.append(matriu[3][0]);
+        result.append(matriu[0][1]);
+        result.append(matriu[1][1]);
+        result.append(matriu[2][1]);
+        System.out.println(result.toString());
+        return result.toString();
     }
+
+    /*static char[][] transpose(char[][] matriu, int alt, int dim){
+        StringBuilder res = new StringBuilder();
+
+        char[][] trans = new char[alt][dim];
+        for (int i = 0; i < matriu.length; i++) {
+            for (int j = 0; j < matriu[i].length; j++) {
+                res.append(i);
+            }
+        }
+        System.out.println(res.toString());
+        return trans;
+    }*/
 
     static String cypher(String s, String key) {
         return "";
